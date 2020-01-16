e2baf3dfa92ff70293d34f1e932a4b609bc0b0e8
birshert <birshertalexey21@gmail.com>
2020-01-16 13:46:06 +0300

    checker on resnet34

--- checker.py
+++ checker.py
@@ -3,6 +3,11 @@ from typing import Any
 import cv2.cv2 as cv2
 import joblib
 import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from PIL import Image
 from skimage.feature import local_binary_pattern
 
 
@@ -31,29 +36,35 @@ class BaseModel:
 
 
 class ModelMem(BaseModel):
-    def __init__(self, margin: float = 0.5, threshold: float = 0.05):
+    def __init__(self, image_size: int = 64):
         super().__init__()
-        assert margin > 0, 'Margin should be greater than 0'
-        assert threshold > 0, 'Threshold should be greater than 0'
-        self.crop_margin = margin
-        self.threshold = threshold
+        transforms_list = []
+        transforms_list.extend([
+            transforms.Resize((image_size, image_size)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225]),
+        ])
+        self.transforms = transforms.Compose(transforms_list)
     
     def transform_data(self, image: np.ndarray) -> np.ndarray:
-        MAX_IMG_SIZE = 200
-        h, w = image.shape[0], image.shape[1]
-        new_h, new_w = int(np.floor(h * self.crop_margin)), int(np.floor(w * self.crop_margin))
-        image = cv2.resize(image, (new_w, new_h))
-        k = h / w
-        if h > MAX_IMG_SIZE or w > MAX_IMG_SIZE:
-            if k >= 1:
-                image = cv2.resize(image, (MAX_IMG_SIZE, int(MAX_IMG_SIZE * k)))
-            else:
-                image = cv2.resize(image, (int(MAX_IMG_SIZE * k), MAX_IMG_SIZE))
-        return image.reshape(-1, 3)
+        return self.transforms(Image.fromarray(image))
+    
+    def _predict(self, data):
+        pred = self.est(data[None, ...])
+        return pred.cpu().data.max(1, keepdim=True)[1]
     
     def predict(self, image: np.ndarray):
         self.check_model_implemented()
-        return self._predict(self.transform_data(image)).mean() >= self.threshold
+        return self._predict(self.transform_data(image))
+    
+    def load_est(self, path):
+        self.est = torchvision.models.resnet34()
+        self.est.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.est.fc = nn.Linear(self.est.fc.in_features, 2)
+        self.est.load_state_dict(torch.load(path))
+        self.est.eval()
+        return self
 
 
 class ModelAds(BaseModel):
@@ -65,7 +76,7 @@ class ModelAds(BaseModel):
     
     def get_lbp_hist_res(self, image: np.ndarray, r: int = 2) -> np.ndarray:
         features = local_binary_pattern(image, self.p * r, r, method="uniform").ravel()
-        bins = int(features.max()) + 1
+        bins = self.p * r + 2  # because of uniform method
         counts, _ = np.histogram(features, bins=bins)
         return counts
     
@@ -79,8 +90,8 @@ class ModelAds(BaseModel):
         for i in range(3):
             YCbCr += list(self.get_lbp_hist_res(img_ycbcr[:, :, i]))
             HSV += list(self.get_lbp_hist_res(img_hsv[:, :, i]))
-        
-        return np.array(YCbCr + HSV)
+
+        return np.array(YCbCr + HSV).reshape(1, -1)
     
     def predict(self, image: np.ndarray):
         self.check_model_implemented()
@@ -88,8 +99,8 @@ class ModelAds(BaseModel):
 
 
 class LivenessChecker:
-    def __init__(self, threshold: float = 0.05, path1: str = 'model1.pkl', path2: str = 'model2.pkl'):
-        self.model_mem = ModelMem(threshold).load_est(path1)
+    def __init__(self, path1: str = 'model1.pth', path2: str = 'model2.pkl'):
+        self.model_mem = ModelMem().load_est(path1)
         self.model_ads = ModelAds().load_est(path2)
     
     def check_liveness(self, image: np.ndarray) -> bool:
